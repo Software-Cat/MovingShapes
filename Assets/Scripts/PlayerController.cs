9ef5bd837a83ef718046b543697b0309c29f42c9
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-07-03 17:02:54 +1000

    Implement character controll

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,37 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
+    [SerializeField] protected Camera playerCam;
     [SerializeField] protected float moveSpeed = 6f;
-    protected Vector2 moveInput;
-    protected CharacterController controller;
+    [SerializeField] protected float turnSmoothness = 0.1f;
+
+    protected CharacterController playerController;
+
+    private Vector2 moveInput;
+    private float turnSmoothVelocity;
+    private Vector3 velocity;
+
+    [Header("Jumping")]
+    [SerializeField] protected float jumpHeight = 3f;
+
+    private bool jumpInput = false;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (playerController.isGrounded && context.started)
+        {
+            jumpInput = true;
+        }
+    }
+
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        playerController = GetComponent<CharacterController>();
     }
 
     // Start is called before the first frame update
@@ -32,8 +51,34 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
 
-        controller.Move(moveSpeed * Time.deltaTime * moveDir);
+        if (direction.magnitude >= 0.1f)
+        {
+            // Turning
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothness);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            // Moving
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            playerController.Move(moveSpeed * Time.deltaTime * moveDir.normalized);
+        }
+
+        // Gravity
+        if (playerController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        velocity.y += Registry.GRAVITY * Time.deltaTime;
+        playerController.Move(velocity * Time.deltaTime);
+
+        // Jumping
+        if (jumpInput)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * Registry.GRAVITY);
+            jumpInput = false;
+        }
     }
 }
--- Assets/Scripts/Registry.cs
+++ Assets/Scripts/Registry.cs
@@ -6,4 +6,6 @@ public static class Registry
 {
     public const int START_SCENE = 0;
     public const int MAIN_SCENE = 1;
+
+    public const float GRAVITY = -9.81f;
 }
